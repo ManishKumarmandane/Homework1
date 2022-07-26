bd1a87a053c08a1c87d54f42bd68f3003028f571
manishkumarmandane <bm0369@gmail.com>
2022-07-26 19:45:39 +0100

    Initial commit

---
+++ src/main/java/HomeVoteage/Voteage.java
@@ -0,0 +1,28 @@
+
+package HomeVoteage;
+
+public class Voteage
+{
+
+        public static void main(String[] args)
+        {
+            int age=19;
+            if (age <=5)
+            {
+                System.out.println("pls register your bady");
+            }
+            else if (age <= 18)
+            {
+                System.out.println("You are not eligible for vote.");
+            }
+            else if(age<=75)
+            {
+                System.out.println("You are  eligible for vote.");
+            }
+            else
+            {
+                System.out.println("Person expries");
+            }
+        }
+
+}
